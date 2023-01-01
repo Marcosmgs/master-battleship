f941ddf52b0f797452be69da100c1a6e48256551
marcosmgs <marcosmgs2023@gmail.com>
2023-01-01 18:34:28 +0000

    Create GameBoard class and add the init method

--- run.py
+++ run.py
@@ -2,5 +2,23 @@ from random import randint
 
 scores = {"computer": 0, "player": 0}
 
-#print(scores)
-#print(randint(0, 7))
+
+class GameBoard:
+    """
+    Main game class, sets the board size, number of ships, 
+    players name and the game board type for computer or player
+    Contains methods for adding ships, guesses, players inputs
+    and printing board.
+    """
+
+    def __init__(self, board_size, num_ships, name, type):
+        self.board_size = board_size
+        self.board = ["." for x in range(board_size) for y in range(board_size)]
+        self.num_ships = num_ships
+        self.name = name
+        self.type = type
+        self.guesses = []
+        self.ships = []
+    
+data = GameBoard(5, 4, "Marcos", type = "player")
+print(data.board_size)
